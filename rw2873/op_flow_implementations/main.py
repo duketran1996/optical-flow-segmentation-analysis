8834793c67318482004376d92690572cb7fea06b
OperaRuss <68122626+OperaRuss@users.noreply.github.com>
2021-05-12 14:58:54 -0400

    Threshold of Marple 13 (whole sequence).  Find in results folder.

--- rw2873/op_flow_implementations/main.py
+++ rw2873/op_flow_implementations/main.py
@@ -15,17 +15,52 @@ import track
 import os
 
 def main():
-    path = './sources/images/FBMS_marple13'
-    directory = os.fsencode(path)
+    tracks = []
+    frames = []
 
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith('.jpg'):
-            img = image_functions.open_image(path + '/' + filename)
-            im_eig = image_functions.threshold_eigenvalues(img, 0.2, 5)
-            image_functions.output_image(im_eig, 'eig_' + filename)
-        else:
-            continue
+    flow_fore = np.zeros(frames[0].shape, dtype=np.float32)
+    flow_back = np.zeros(frames[0].shape, dtype=np.float32)
+
+    for frame in range(0,len(frames)):
+        # Calculate the forward and backwards flow between the current and next frame
+        flow_fore = cv.calcOpticalFlowFarneback(frames[frame], frames[frame + 1], flow_fore,
+                                                0.5, 5, 5, 5, 5, 1.1, cv.OPTFLOW_FARNEBACK_GAUSSIAN)
+        flow_back = cv.calcOpticalFlowFarneback(frames[frame + 1], frames[frame], flow_back,
+                                                0.5, 5, 5, 5, 5, 1.1, cv.OPTFLOW_FARNEBACK_GAUSSIAN)
+
+        # Check if new objects are in scene in the FORWARD FLOW
+        for row in flow_fore:
+            for col in flow_fore:
+                # check each pixel for flow response
+
+                # if there is a response > than a certain threshold
+                for track in tracks: # check track.curr_pos
+                # if no track is currently tracking the point, create a new one
+
+        for curr_track in tracks:
+            fwd_flow = flow_fore[curr_track.curr_position[0]][curr_track.curr_position[1]]
+
+            # New position is (x + u, y + v)
+            new_pos = (curr_track.curr_position[0] + fwd_flow[0], curr_track.curr_position[1] + fwd_flow[1])
+
+
+            bck_flow_u = # BILINEAR INTERPOLATION ON NEW_POS ON U
+            bck_flow_v = # BILINEAR INTERPOLATION ON NEW_POST ON V
+
+            # Check to make sure they match (using the W equation)
+
+            # Update & append position if they do, kill if not
+
+    # STEP 3) Construct affinity matrix
+
+    # STEP 4) Populate affinity values
+
+    # Step 5) Spectral Clustering
+
+
+
+
+
 
     return 0
 
